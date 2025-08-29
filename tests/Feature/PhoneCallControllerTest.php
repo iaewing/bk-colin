c42384ffd0ca44d60b3003cad5fccaf1697389ae
Ian Ewing <ian@ewing.codes>
2025-08-29 09:28:16 -0400

    swap to Pest

--- tests/Feature/PhoneCallControllerTest.php
+++ tests/Feature/PhoneCallControllerTest.php
@@ -1,44 +1,31 @@
 <?php
 
-namespace Tests\Feature;
-
-use Tests\TestCase;
 use Illuminate\Support\Facades\Storage;
 
-class PhoneCallControllerTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+test('it records details of the incoming call', function () {
 
-        $response->assertStatus(200);
-    }
+});
 
-    public function test_answer_returns_valid_ncco()
-    {
-        $response = $this->post('/api/voice/answer');
+test('answer returns valid ncco', function () {
+    $response = $this->post('/api/voice/answer');
 
-        $response->assertStatus(200);
-        $response->assertJsonStructure([
-            [
-                'action' => 'stream',
-                'streamUrl' => Storage::disk('colin_audio')->url('hello.wav')
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        [
+            'action' => 'stream',
+            'streamUrl' => Storage::disk('colin_audio')->url('hello.wav')
+        ],
+        [
+            'action' => 'input',
+            'type' => ['speech'],
+            'speech' => [
+                'endOnSilence' => 2
             ],
-            [
-                'action' => 'input',
-                'type' => ['speech'],
-                'speech' => [
-                    'endOnSilence' => 2
-                ],
-                'eventUrl' => 'https://bk-colin.test/voice/event'
-            ],
-            [
-                'action' => 'stream',
-                'streamUrl' => Storage::disk('colin_audio')->url('feedback.wav')
-            ]
-        ]);
-    }
-}
+            'eventUrl' => 'https://bk-colin.test/voice/event'
+        ],
+        [
+            'action' => 'stream',
+            'streamUrl' => Storage::disk('colin_audio')->url('feedback.wav')
+        ]
+    ]);
+});
