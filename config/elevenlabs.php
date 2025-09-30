59ae6222a670e28378027dd448228535f6d94153
Ian Ewing <ian@ewing.codes>
2025-09-30 09:43:57 -0400

    update env name

--- config/elevenlabs.php
+++ config/elevenlabs.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_key' => env('ELEVENLABS_API_KEY'),
+    'api_key' => env('ELEVEN_LABS_API_KEY'),
     'colin_voice_id' => env('ELEVEN_LABS_COLIN_VOICE_ID', 'colin_420_69'),
 ];
